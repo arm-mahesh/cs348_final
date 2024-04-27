61a27a5f1b67e21c88128266afcfa189babbf6e7
arm-mahesh <armanyamaheshwari@gmail.com>
2024-04-26 22:00:33 -0400

    port

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 from flask import Flask, request, render_template, url_for, redirect
 from peewee import *
 
@@ -179,4 +179,5 @@ def delete_recipe(recipe_id):
         return "Recipe not found", 404
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
